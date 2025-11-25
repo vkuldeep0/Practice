663010ceaea9b388062da73c808562427f85a7dc
vkuldeep0 <kv773708@gmail.com>
2025-11-25 19:17:22 +0530

    Added new app.py

---
+++ FlaskGunicornDemo/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return jsonify({
+		"message": "Hello from Flask + Gunicorn!"
+		"worker": os.getenv.get("WORKER_ID", "unknown")
+	})
+
+@app.route("/echo", methods=["POST"])
+def echo():
+	data = request.json or {}
+	return jsonify({"you_sent": data})
